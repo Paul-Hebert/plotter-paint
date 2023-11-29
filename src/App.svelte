9bc0742087e349bb58b67bce2dbf3f7776a10525
Paul Hebert <paul@cloudfour.com>
2023-11-28 22:37:18 -0800

    Support basic line drawing

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,54 @@
 <script lang="ts">
+  import { spline } from '@georgedoescode/spline'
   let svgCanvas;
 
-  let circles = [];
+  let strokes = [];
+  let isDrawing = false;
+  let pointsInCurrentStroke = [];
+  let animationFrame;
 
-  function addCircle(e) {
-    const { x,y } = getClickOnSvg(e);
+  function handlePointerDown(e) {
+    isDrawing = true;
+    pointsInCurrentStroke.push(getClickOnSvg(e));
 
-    circles = [...circles, {x, y}]
+    strokes.push({
+      points: pointsInCurrentStroke,
+    });
+
+    console.log(strokes);
   }
 
+  function handlePointerMove(e) {
+    if (isDrawing) {
+      if (animationFrame) {
+        cancelAnimationFrame(animationFrame);
+      }
 
-  function getClickOnSvg(e) {
+      animationFrame = requestAnimationFrame(() => {
+        pointsInCurrentStroke.push(getClickOnSvg(e));
+
+        strokes.pop()
+        strokes = [...strokes, {points: pointsInCurrentStroke}]
+
+        console.log(strokes);
+      });
+    }
+  }
+
+  function handlePointerUp() {
+    if (animationFrame) {
+      cancelAnimationFrame(animationFrame);
+    }
+
+    isDrawing = false;
+    pointsInCurrentStroke = [];
+  }
+
+  function getClickOnSvg({clientX, clientY}) {
     let pt = DOMPoint.fromPoint(svgCanvas); 
-    pt.x = e.clientX;
-    pt.y = e.clientY;
-    
+    pt.x = clientX;
+    pt.y = clientY;
+
 
     // The cursor point, translated into svg coordinates
     return  pt.matrixTransform(svgCanvas.getScreenCTM().inverse());
@@ -23,9 +57,15 @@
 </script>
 
 <main>
-  <svg viewBox="0 0 500 500" on:click={addCircle} bind:this={svgCanvas}>
-    {#each circles as { x, y }}
-      <circle cx="{x}" cy="{y}" r="5"/> 
+  <svg 
+    viewBox="0 0 500 500" 
+    on:pointerdown={handlePointerDown} 
+    on:pointermove={handlePointerMove} 
+    on:pointerup={handlePointerUp} 
+    bind:this={svgCanvas}
+  >
+    {#each strokes as { points }}
+      <path d="{spline(points)}"/> 
     {/each}
   </svg>
 </main>
@@ -36,5 +76,11 @@
     border: 1px solid #ccc;
     width: 500px;
     height: 500px;
+  }
+
+  path {
+    fill: none;
+    stroke: #000;
+    stroke-width: 5;
   }
 </style>
